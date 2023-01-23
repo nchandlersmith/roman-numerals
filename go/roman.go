0ab0245c10429353f671f6664f520d1dd30e3be5
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2023-01-22 19:39:34 -0600

    Refactor

--- go/roman.go
+++ go/roman.go
@@ -1,29 +1,26 @@
 package roman
 
 func ToRoman(number int) string {
-	if number == 4 {
-		return "IV"
-	}
-	if number == 9 {
-		return "IX"
-	}
-	if number == 14 {
-		return "XIV"
-	}
-	if number == 19 {
-		return "XIX"
-	}
 	numeral := ""
 	for number >= 10 {
 		numeral += "X"
 		number -= 10
 	}
+	if number == 9 {
+		numeral += "IX"
+		number -= 9
+	}
 	if number >= 5 {
 		numeral += "V"
 		number -= 5
 	}
-	for i := 1; i <= number; i++ {
+	if number == 4 {
+		numeral += "IV"
+		number -= 4
+	}
+	for number < 4 && number > 0 {
 		numeral += "I"
+		number -= 1
 	}
 	return numeral
 }
